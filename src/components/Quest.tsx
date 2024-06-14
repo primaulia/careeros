328fbfb29581555174ce0b0e9f0e2f63c7ba135f
Prima Aulia Gusta <prima@tablevibe.co>
2024-06-15 01:00:03 +0800

    add install button

--- src/components/Quest.tsx
+++ src/components/Quest.tsx
@@ -1,9 +1,22 @@
 import { QuestType } from "../types/QuestType";
 import "./Quest.scss";
 
+import install_extension from "../images/install_extension.png";
+
+const ActionButton = ({ type }: { type: string }) => {
+  if (type.includes("install")) {
+    return (
+      <button className="card__action card__action--install">Install </button>
+    );
+  }
+
+  return <h1>{type}</h1>;
+};
+
 export const Quest = ({ quest }: { quest: QuestType }) => {
   return (
     <div className="card">
+      <img className="card__icon" src={install_extension} alt="Test" />
       <div>
         <h3 className="card__title">{quest.type}</h3>
         <p className="card__description">
@@ -13,6 +26,7 @@ export const Quest = ({ quest }: { quest: QuestType }) => {
           send Linkedin messages directly from CareerOS.
         </p>
       </div>
+      <ActionButton type={quest.type} />
     </div>
   );
 };
