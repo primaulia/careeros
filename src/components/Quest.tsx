8987b51bd766611b21be2fbd32ba698d996af752
Prima Aulia Gusta <prima@tablevibe.co>
2024-06-15 02:05:20 +0800

    setup data parser for the raw json data

--- src/components/Quest.tsx
+++ src/components/Quest.tsx
@@ -1,13 +1,22 @@
 import { QuestType } from "../types/QuestType";
 import "./Quest.scss";
 
-import install_extension from "../images/install_extension.png";
+import installExtension from "../images/installExtension.png";
 import todo from "../images/todo.png";
+import installIcon from "../images/installIcon.png";
+import { parser } from "../utils/dataParser";
 
 const ActionButton = ({ type }: { type: string }) => {
   if (type.includes("install")) {
     return (
-      <button className="card__action card__action--install">Install </button>
+      <button className="card__action card__action--install">
+        Install{" "}
+        <img
+          className="card__action--installIcon"
+          src={installIcon}
+          alt="install icon"
+        />
+      </button>
     );
   }
 
@@ -17,27 +26,20 @@ const ActionButton = ({ type }: { type: string }) => {
         console.log("click todo link");
       }}
     >
-      <img
-        className="card__action card__action--todolink"
-        src={todo}
-        alt="Todo link"
-      />
+      <img src={todo} alt="Todo link" />
     </a>
   );
 };
 
 export const Quest = ({ quest }: { quest: QuestType }) => {
+  const parsedData = parser(quest);
+
   return (
     <div className="card">
-      <img className="card__icon" src={install_extension} alt="Test" />
+      <img className="card__icon" src={parsedData!.icon} alt="Test" />
       <div>
-        <h3 className="card__title">{quest.type}</h3>
-        <p className="card__description">
-          Unlock the superpowers of CareerOS with our Chrome Extension. This
-          plugin lets you save companies, contacts, and jobs from Linkedin and
-          other job boards. It also lets you access our messaging template and
-          send Linkedin messages directly from CareerOS.
-        </p>
+        <h3 className="card__title">{parsedData!.title}</h3>
+        <p className="card__description">{parsedData!.description}</p>
       </div>
       <ActionButton type={quest.type} />
     </div>
---
+++ src/utils/dataParser.ts
@@ -0,0 +1,97 @@
+import { QuestType } from "../types/QuestType";
+import installExtension from "../images/installExtension.png";
+
+const contactFullname = (contact: { first_name: string; last_name: string }) =>
+  contact.first_name + " " + contact.last_name;
+
+export const parser = (data: QuestType) => {
+  const { type, due_date, company, contact, job } = data;
+
+  switch (type) {
+    case "install_extension":
+      return {
+        title: "Install the CareerOS Chrome Extension",
+        description:
+          "Don`t forget buy a wUnlock the superpowers of CareerOS with our Chrome Extension. This plugin lets you save companies, contacts, and jobs from Linkedin and other job boards. It also lets you access our messaging template and send Linkedin messages directly from CareerOS.",
+        icon: installExtension,
+      };
+    case "submit_application":
+      return {
+        title: `Submit your application for ${job!.title} at ${company!.name}`,
+        description: `Deadline ${due_date}`,
+        icon: installExtension,
+      };
+    case "resolve-comment":
+      return {
+        title: `Resolve a comment from [CA name]`,
+        description: "You have an unresolved comment from your Career Advisor.",
+        icon: installExtension,
+      };
+    case "save_first_company":
+      return {
+        title: `Save your first company`,
+        description:
+          "Browse our company database for your favorite companies or new inspiration and save your first company to get started.",
+        icon: installExtension,
+      };
+    case "add_contact":
+      return {
+        title: `Save a contact at ${company!.name}`,
+        description:
+          "Browse our company database for your favorite companies or new inspiration and save your first company to get started.",
+        icon: company?.logo_url,
+      };
+    case "send_linkedin_message":
+      return {
+        title: `Send a LinkedIn message to ${contactFullname(contact!)}  at ${
+          company!.name
+        }`,
+        description:
+          "Send a customized LinkedIn message to reach out to your new contact. Don't worry! We'll help you with the wording.",
+        icon: contact!.profile_image,
+      };
+    case "send_linkedin_connection":
+      return {
+        title: `Send a LinkedIn invitation to ${contactFullname(
+          contact!
+        )}  at ${company!.name}`,
+        description:
+          "Send an invitation with a tailored message to your new contact. Our AI will help you!",
+        icon: contact!.profile_image,
+      };
+    case "send_first_email":
+      return {
+        title: `Send first email to ${contactFullname(contact!)}  at ${
+          company!.name
+        }`,
+        description: `Send a first email to ${contactFullname(contact!)} at ${
+          company!.name
+        } to get a conversation started.`,
+        icon: contact!.profile_image,
+      };
+    case "add_new_contact":
+      return {
+        title: `Save a new contact at ${company!.name}`,
+        description: "Add another contact to this company to start networking.",
+        icon: company?.logo_url,
+      };
+    case "schedule_coffee_chat":
+      return {
+        title: `Schedule a coffee chat with ${contactFullname(contact!)} at ${
+          company!.name
+        }`,
+        description:
+          "Get unique insights from your new contact by scheduling an informational interview with them. Make sure to use our coffee chat tips to prepare for your meeting.",
+        icon: installExtension,
+      };
+    case "send_follow_up_email":
+      return {
+        title: `Send follow-up email to ${contactFullname(contact!)} at ${
+          company!.name
+        }`,
+        description:
+          "Send a follow-up email to [contact name] at [company] to get a conversation started.",
+        icon: contact!.profile_image,
+      };
+  }
+};
